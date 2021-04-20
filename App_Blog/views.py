45b82f522293e758a4bb2f5b437359ce88938d69
sakil-islam <77716390+sakil-islam@users.noreply.github.com>
2021-04-20 23:49:54 +0600

    Blog project Like and Unlike Add

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -31,11 +31,11 @@ class BlogList(ListView):
 def blog_details(request, slug):
     blog = Blog.objects.get(slug=slug)
     comment_form = CommentForm()
-    # already_liked = Likes.objects.filter(blog=blog, user= request.user)
-    # if already_liked:
-    #     liked = True
-    # else:
-    #     liked = False
+    already_liked = Likes.objects.filter(blog=blog, user= request.user)
+    if already_liked:
+        liked = True
+    else:
+        liked = False
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -44,5 +44,31 @@ def blog_details(request, slug):
             comment.blog = blog
             comment.save()
             return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':slug}))
-    #, 'liked':liked,
-    return render(request, 'App_Blog/blog_details.html', context={'blog':blog, 'comment_form':comment_form})
+    return render(request, 'App_Blog/blog_details.html', context={'blog':blog, 'comment_form':comment_form, 'liked':liked})
+
+
+@login_required
+def liked(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Likes.objects.filter(blog=blog, user=user)
+    if not already_liked:
+        liked_post = Likes(blog=blog, user=user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':blog.slug}))
+#
+@login_required
+def unliked(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Likes.objects.filter(blog=blog, user=user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':blog.slug}))
+
+# class UpdateBlog(LoginRequiredMixin, UpdateView):
+#     model = Blog
+#     fields = ('blog_title', 'blog_content', 'blog_image')
+#     template_name = 'App_Blog/edit_blog.html'
+#
+#     def get_success_url(self, **kwargs):
+#         return reverse_lazy('App_Blog:blog_details', kwargs={'slug':self.object.slug})
