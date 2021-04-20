eaa0fa3ee1c7530c1229a8550108d215a3efc41d
sakil-islam <77716390+sakil-islam@users.noreply.github.com>
2021-04-21 00:35:19 +0600

    Blog project bloger blog edit, delete all blogs design

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from App_Blog.forms import CommentForm
 import uuid
 # Create your views here.
+class MyBlogs(LoginRequiredMixin, TemplateView):
+    template_name = 'App_Blog/my_blogs.html'
 
 class CreateBlog(LoginRequiredMixin, CreateView):
     model = Blog
@@ -65,10 +67,10 @@ def unliked(request, pk):
     already_liked.delete()
     return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':blog.slug}))
 
-# class UpdateBlog(LoginRequiredMixin, UpdateView):
-#     model = Blog
-#     fields = ('blog_title', 'blog_content', 'blog_image')
-#     template_name = 'App_Blog/edit_blog.html'
-#
-#     def get_success_url(self, **kwargs):
-#         return reverse_lazy('App_Blog:blog_details', kwargs={'slug':self.object.slug})
+class UpdateBlog(LoginRequiredMixin, UpdateView):
+    model = Blog
+    fields = ('blog_title', 'blog_content', 'blog_image')
+    template_name = 'App_Blog/edit_blog.html'
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('App_Blog:blog_details', kwargs={'slug':self.object.slug})
